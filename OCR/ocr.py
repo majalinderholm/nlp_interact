0c33c301fef3e8ad4ae0654204c88b79ae18e46a
Maja Linderholm <maja.linderholm@gmail.com>
2021-06-08 13:21:24 +0200

    Now we're able to perform OCR on a single file and can get the text and the confidence percentages

--- OCR/ocr.py
+++ OCR/ocr.py
@@ -1,13 +1,48 @@
 import os, io
 from google.cloud import vision_v1 as vision
 from google.cloud.vision_v1 import types
+import pandas as pd
 
+def detect_text(img):
+    # Open image
+    with open(img, 'rb') as image_file:
+        content = image_file.read()
+
+    #Define input and perform ocr to get the response
+    image = vision.types.Image(content=content)
+    response = client.document_text_detection(
+                                            image=image,
+                                            image_context = {"language_hints": ['sv']} #swedish text
+                                            )
+
+    return response
+
+def get_confidence(pages):
+    for page in pages:
+        for block in page.blocks:
+            print('block confidence:', block.confidence)
+
+            for paragraph in block.paragraphs:
+                print('paragraph confidence:', paragraph.confidence)
+
+                for word in paragraph.words:
+                    word_text = ''.join([symbol.text for symbol in word.symbols])
+                    print('Word text: {0} (confidence: {1}'.format(word_text, word.confidence))
+
+                    for symbol in word.symbols:
+                        print('\tSymbol: {0} (confidence: {1}'.format(symbol.text, symbol.confidence))
+
+# Provide the API with the Google Cloud Vision API
 os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = r'C:/Users/paw472/OneDrive - AFRY\Documents/Projects/nlp_interact/key_vision.json'
 
+# Initialize a Google Cloud Vision client
 client = vision.ImageAnnotatorClient()
 
-FILE_NAME = 'DSC_0104.NEF'
-FOLDER_PATH = r'C:/Users/paw472/OneDrive - AFRY/Documents/Projects/nlp_interact/DataCollection/ValterSchytt-2-1962'
-
-with io.open(os.path.join(FOLDER_PATH, FILE_NAME), 'rb') as image_file:
-    content = image_file.read()
+# Define file name and path
+# FILE_NAME = 'DSC_0104.NEF'
+# FOLDER_PATH = r'C:/Users/paw472/OneDrive - AFRY/Documents/Projects/nlp_interact/DataCollection/ValterSchytt-2-1962'
+FILE_NAME = 'IMG_4966.jpg'
+FOLDER_PATH = r'C:/Users/paw472/OneDrive - AFRY/Documents/Projects/nlp_interact'
+response = detect_text(os.path.join(FOLDER_PATH, FILE_NAME))
+print(response.full_text_annotation.text)
+#get_confidence(response.full_text_annotation.pages)
