edb1817aa5e0815c181179920f20a27e5e246ad5
Maja Linderholm <maja.linderholm@gmail.com>
2021-06-18 13:19:23 +0200

    The algorithm is now able to summarize from a .txt-file

--- Summarization/summarization.py
+++ Summarization/summarization.py
@@ -1,5 +1,24 @@
 import os
 import openai
+
+#Access the API-key
 key = open("C:/Users/paw472/OneDrive - AFRY/Documents/Projects/nlp_interact/Keys/key_gpt3.txt", "r")
 openai.api_key = key.read()
-#print(openai.Engine.retrieve("davinci"))
+
+#Open the file containing text that is about to be summarized
+file = open("Summarization/prompt.txt", "r")
+prompt = file.read()
+print(prompt)
+
+response = openai.Completion.create(
+                                    engine = "davinci",
+                                    prompt = prompt,
+                                    temperature = 0,
+                                    max_tokens = 200,
+                                    top_p = 1.0,
+                                    frequency_penalty = 0.0,
+                                    presence_penalty = 0.0,
+                                    stop=["\n\"\"\"n"]
+                                    )
+
+print(response.choices)
